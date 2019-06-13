4897696c5289217ad906c9e4eca2af33fc7da716
r <michalski.rados@gmail.com>
2019-06-13 17:41:55 +0200

    calculator.html modified

---
+++ src/test/java/com/radek/simplecalculator/service/CalculationServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.radek.simplecalculator.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculationServiceImplTest {
+
+    @Test
+    public void addition() {
+    }
+
+    @Test
+    public void subtraction() {
+    }
+
+    @Test
+    public void division() {
+    }
+
+    @Test
+    public void multiplication() {
+    }
+
+    @Test
+    public void exponentiation() {
+    }
+
+    @Test
+    public void squareRoot() {
+    }
+}
